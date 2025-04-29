33fe8d111ccfd2c971906a07de6342860c789369
严张鑫 <yan.zhangxin@student.zy.cdut.edu.cn>
2025-04-29 20:53:21 +0800

    update 5

--- core/PEModifier.py
+++ core/PEModifier.py
@@ -5,17 +5,17 @@ class PEModifier:
         self.output = output_file
 
     # 读取PE文件并且将IP地址和端口内容修改
-    def modify_pe_hex_string(self, start_addr, end_addr, new_hex_string):
+    def modify_pe_hex_string(self, start_addr, end_addr, bytes_provided):
 
-        try:
-            new_bytes = bytes.fromhex(new_hex_string)
-        except ValueError as e:
-            print(f"无效的十六进制字符串: {e}")
-            return
+        # try:
+        #     new_bytes = bytes.fromhex(new_hex_string)
+        # except ValueError as e:
+        #     print(f"无效的十六进制字符串: {e}")
+        #     return
 
         # 计算需要修改的长度
-        original_length = end_addr - start_addr + 1
-        new_length = len(new_bytes)
+        original_length = end_addr - start_addr
+        new_length = len(bytes_provided)
 
         if new_length != original_length:
             print(f"警告: 新数据长度({new_length})与原数据长度({original_length})不同")
@@ -34,7 +34,7 @@ class PEModifier:
                 return
 
             # 替换数据
-            data[start_addr:end_addr + 1] = new_bytes
+            data[start_addr:end_addr + 1] = bytes_provided
 
             # 写入新文件
             with open(self.output, 'wb') as f:
